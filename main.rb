3e10b30de15da6a3f87c43e7cf70f9fb50eb9964
Yuki Matsukura <matsubokkuri@gmail.com>
2019-08-27 10:26:58 +0900

    rubocop -a

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'logger'
 require './Downloader'
 #
@@ -11,31 +13,28 @@ task = []
 min_page = 1
 max_page = 1
 
-
 min_page.upto(max_page) { |i| task << i }
 
 # number of concurrency
 concurrency = 5
 
 threads = []
-1.upto(concurrency) {
-    threads << Thread.new do
-        while true
-            break if task.count() == 0
-            begin
-                page = task.pop()
-                #downloader = Downloader.new({ :proxy => 'http://212.90.161.214:3128/', :logger => logger })
-                downloader = Downloader.new({ :logger => logger })
-                downloader.parseList(page)
-            rescue RuntimeError => e
-                logger.warn e.to_s
-            end
-        end
-    end
-    sleep 0.1
-}
+1.upto(concurrency) do
+  threads << Thread.new do
+    loop do
+      break if task.count == 0
 
+      begin
+        page = task.pop
+        # downloader = Downloader.new({ :proxy => 'http://212.90.161.214:3128/', :logger => logger })
+        downloader = Downloader.new(logger: logger)
+        downloader.parseList(page)
+      rescue RuntimeError => e
+        logger.warn e.to_s
+      end
+    end
+  end
+  sleep 0.1
+end
 
 (Thread.list - [Thread.current]).each &:join
-
-
